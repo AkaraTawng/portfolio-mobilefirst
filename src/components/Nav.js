56bd47ac6f30ea7b4fe5f554309c8ba89754288d
Christopher Chamberlain <c.r.chamberlain1997@gmail.com>
2022-07-20 08:53:40 +0700

    wrap lis with navlink in Nav.js

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -7,24 +7,30 @@ export default function Nav() {
     return <>
     <nav id="nav">
         <ul>
-            <li>
-               <a className="internal-links" href="#">Home</a> 
-            </li>
-            <li>
-               <a className="internal-links" href="#">Projects</a> 
-            </li>
-            <li>
-               <a className="internal-links" href="#">Contact</a> 
-            </li>
-            <li>
-               <a  className="internal-links" href="#">Resume</a> 
-            </li>
-            <li>
-               <a href="#"><BsGithub size={35}/></a> 
-            </li>
-            <li>
-               <a id="linkedin" href="#"><BsLinkedin size={35}/></a> 
-            </li>
+            <NavLink>
+                <li>
+                <a className="internal-links" href="#">Home</a> 
+                </li>
+            </NavLink>
+            <NavLink>
+                <li>
+                <a className="internal-links" href="#">Projects</a> 
+                </li>
+            </NavLink>
+            <NavLink>
+                <li>
+                <a className="internal-links" href="#">Contact</a> 
+                </li>
+            </NavLink>
+                <li>
+                <a  className="internal-links" href="#">Resume</a> 
+                </li>
+                <li>
+                <a href="#"><BsGithub size={35}/></a> 
+                </li>
+                <li>
+                <a id="linkedin" href="#"><BsLinkedin size={35}/></a> 
+                </li>
         </ul>
     </nav>
     </>
