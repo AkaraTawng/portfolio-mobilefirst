73cf84f7609a114f2332c9fd4ec748e217d952f3
Christopher Chamberlain <c.r.chamberlain1997@gmail.com>
2022-07-20 07:43:44 +0700

    add ul and li items

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,5 +3,28 @@ import {BsGithub} from "react-icons/bs";
 import {BsLinkedin} from "react-icons/bs";
 
 export default function Nav() {
-    return ;
+    return <>
+    <nav id="nav">
+        <ul>
+            <li>
+               <a className="internal-links" href="#">Home</a> 
+            </li>
+            <li>
+               <a className="internal-links" href="#">Projects</a> 
+            </li>
+            <li>
+               <a className="internal-links" href="#">Contact</a> 
+            </li>
+            <li>
+               <a  className="internal-links" href="#">Resume</a> 
+            </li>
+            <li>
+               <a href="#"><BsGithub size={35}/></a> 
+            </li>
+            <li>
+               <a id="linkedin" href="#"><BsLinkedin size={35}/></a> 
+            </li>
+        </ul>
+    </nav>
+    </>
 }
