57c51df6784cf1d52c93c200e632be6ab024c5a5
Christopher Chamberlain <c.r.chamberlain1997@gmail.com>
2022-07-20 07:40:53 +0700

    import linkedin and github icons

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import {BsGithub} from "react-icons/bs";
+import {BsLinkedin} from "react-icons/bs";
 
 export default function Nav() {
-    return null;
+    return ;
 }
