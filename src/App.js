4fd9564c00f5663b6ffa328dc54cee9d888226e8
Christopher Chamberlain <c.r.chamberlain1997@gmail.com>
2022-07-23 14:10:51 +0700

    import Projects into App

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import {BrowserRouter, Route, Switch} from 'react-router-dom'; 
 import Nav from './components/Nav';
 import Home from './components/Home';
+import Project from './components/Projects';
 import '../src/main.css';
 
 function App() {
