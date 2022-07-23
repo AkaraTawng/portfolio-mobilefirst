4583faa6fc511e31e658dfaa644b91f0b7ac0838
Christopher Chamberlain <c.r.chamberlain1997@gmail.com>
2022-07-23 14:10:03 +0700

    import Home into App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import {BrowserRouter, Route, Switch} from 'react-router-dom'; 
 import Nav from './components/Nav';
+import Home from './components/Home';
 import '../src/main.css';
 
 function App() {
