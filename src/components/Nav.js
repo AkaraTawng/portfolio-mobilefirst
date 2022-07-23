f73d2b25fba0c263b26e3058d54ad8839964f45c
Christopher Chamberlain <c.r.chamberlain1997@gmail.com>
2022-07-23 13:58:15 +0700

    add linkedin link to linkedin a tag

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -23,7 +23,7 @@ export default function Nav() {
                 <a href="#"><BsGithub size={35}/></a> 
                 </li>
                 <li>
-                <a id="linkedin" href="#"><BsLinkedin size={35}/></a> 
+                <a id="linkedin" href="https://www.linkedin.com/in/christopher-chamberlain-023507205/"><BsLinkedin size={35}/></a> 
                 </li>
         </ul>
     </nav>
