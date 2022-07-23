fd8b2024ef5b70f2bc136a3bc3d81a600b169e5a
Christopher Chamberlain <c.r.chamberlain1997@gmail.com>
2022-07-23 14:11:34 +0700

    import Contact into App

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import Nav from './components/Nav';
 import Home from './components/Home';
 import Project from './components/Projects';
+import Contact from './components/Contact';
 import '../src/main.css';
 
 function App() {
