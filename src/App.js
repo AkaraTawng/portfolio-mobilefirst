c0fa2b0cf30334f4aaa61f27756e0626eae240f9
Christopher Chamberlain <c.r.chamberlain1997@gmail.com>
2022-07-24 18:39:49 +0700

    fix routing imports

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
-import {BrowserRouter, Route, Switch} from 'react-router-dom'; 
+import {BrowserRouter, Route, Routes} from 'react-router-dom'; 
 import Nav from './components/Nav';
 import Home from './components/Home';
 import Project from './components/Projects';
 import Contact from './components/Contact';
+
 import '../src/main.css';
 
 function App() {
-  return (
+  return <>
+    <Nav/>
     <BrowserRouter>
-      <div className="App">
-        <Nav/>
-      </div>
+      <Routes>
+        
+            <route path="/home">
+              <Home/>
+            </route>
+        
+      </Routes>
     </BrowserRouter>
-  );
+    </>
 }
 
 export default App;
